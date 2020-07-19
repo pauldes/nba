81a9b83897d20695f7efb7dafd5ebfea1b136338
pauldes <pauldesigaud@outlook.fr>
2020-07-19 02:12:58 +0200

    Cool stuff

--- nba/analyze.py
+++ nba/analyze.py
@@ -1,6 +1,22 @@
 import numpy
 import seaborn
 
+def get_columns_with_inter_correlations_under(dataframe, treshold):
+    keep = dataframe.columns
+    data_curr = dataframe.copy()
+    initial_num_cols = len(data_curr.columns)
+    corr = get_column_pairs_correlation(data_curr)
+    corr = corr[corr>treshold]
+    while len(corr) > 0:
+        col_to_drop = corr.index.values[0][1]
+        print("Dropping", col_to_drop, "which is correlated with", corr.index.values[0][0])
+        data_curr = data_curr.drop(col_to_drop, axis='columns')
+        corr = get_column_pairs_correlation(data_curr)
+        corr = corr[corr>treshold]
+    res = data_curr.columns
+    print("Reduced number of columns from", initial_num_cols, "to", len(res))
+    return res
+
 def get_column_pairs_correlation(dataframe):
     corr = dataframe.corr().abs()
     s = corr.unstack()
@@ -13,6 +29,12 @@ def get_columns_correlation_with_target(dataframe, target_column):
     res = corr[target_column].abs().sort_values(ascending=False)
     res = res[res<1.000]
     return res
+
+def pairplot_columns(dataframe, columns, color_by):
+    seaborn.pairplot(dataframe, hue=color_by, x_vars=columns, y_vars=columns, corner=True)
+
+def plot_columns_against_target(dataframe, columns, target_column):
+    seaborn.pairplot(dataframe, x_vars=[target_column], y_vars=columns, corner=True)
 
 def plot_correlation_heatmap(dataframe, corner=True):
     # Compute the correlation matrix
--- nba/preprocess.py
+++ nba/preprocess.py
@@ -1,7 +1,33 @@
+import random
+
 import pandas
+import numpy
+import sklearn
+
+def standardize(dataframe, fit_on=None, fit_per_values_of=None):
+    scaler = sklearn.preprocessing.StandardScaler(with_mean=True, with_std=True)
+    if fit_on is None:
+        fit_on = dataframe
+    scaled = dataframe.copy()
+    scaler.fit(fit_on[fit_on.columns])
+    scaled[scaled.columns] = scaler.fit_transform(dataframe[dataframe.columns]) 
+    return scaled
 
 def get_numerical_columns(dataframe):
-    return dataframe.select_dtypes(include="number")
+    return dataframe.select_dtypes(include="number").columns
 
 def get_categorical_columns(dataframe):
-    return dataframe.select_dtypes(exclude="number")
+    return dataframe.select_dtypes(exclude="number").columns
+
+def natural_log_transform(series):
+    return numpy.log(series+1)
+
+def exp_transform(series):
+    return numpy.exp(series)
+
+def select_random_unique_values(series, share:float):
+    seasons = series.unique()
+    share_int = int(share*len(seasons))
+    seasons = random.choices(seasons, k=share_int)
+    print(share_int)
+    return seasons
