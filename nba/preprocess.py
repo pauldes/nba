a7b16b429a91f255de65c670eceec522e39638e5
pauldes <pauldesigaud@outlook.fr>
2020-07-20 16:50:19 +0200

    Gradient boosting

--- nba/preprocess.py
+++ nba/preprocess.py
@@ -6,11 +6,20 @@ import sklearn
 
 def standardize(dataframe, fit_on=None, fit_per_values_of=None):
     scaler = sklearn.preprocessing.StandardScaler(with_mean=True, with_std=True)
+    if fit_on is not None and fit_per_values_of is not None:
+        raise NotImplementedError
     if fit_on is None:
-        fit_on = dataframe
+        fit_on = dataframe.copy()
     scaled = dataframe.copy()
+    if fit_per_values_of is not None:
+        series = fit_per_values_of.copy()
+        for unique in series.unique():
+            curr_index = series[series==unique].index
+            df_subset = dataframe.loc[curr_index, :]
+            scaler.fit(df_subset[df_subset.columns])
+            scaled.loc[curr_index, scaled.columns] = scaler.transform(df_subset[df_subset.columns]) 
     scaler.fit(fit_on[fit_on.columns])
-    scaled[scaled.columns] = scaler.fit_transform(dataframe[dataframe.columns]) 
+    scaled[scaled.columns] = scaler.transform(dataframe[dataframe.columns]) 
     return scaled
 
 def get_numerical_columns(dataframe):
@@ -26,8 +35,7 @@ def exp_transform(series):
     return numpy.exp(series)
 
 def select_random_unique_values(series, share:float):
-    seasons = series.unique()
-    share_int = int(share*len(seasons))
-    seasons = random.choices(seasons, k=share_int)
+    share_int = int(share*len(series.unique()))
+    seasons = series.sample(share_int).tolist()
     print(share_int)
     return seasons
