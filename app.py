2b29eadf75810144274c526e3853c5d3d64908c8
pauldes <pauldesigaud@outlook.fr>
2021-01-31 19:01:28 +0100

    Adjust app

--- app.py
+++ app.py
@@ -29,7 +29,9 @@ def load_team_stats(season):
     return stats
 @st.cache
 def consolidate_stats(team_stats, player_stats):
-    stats = player_stats.merge(team_stats, how='inner', on=["TEAM"])
+    player_stats["SEASON"] = player_stats["SEASON"].astype(int)
+    team_stats["SEASON"] = team_stats["SEASON"].astype(int)
+    stats = player_stats.merge(team_stats, how='inner', on=["TEAM", "SEASON"])
     #stats = stats.set_index("player_season_team", drop=True)
     stats.to_csv("./data/current_consolidated_raw.csv")
     return stats
@@ -46,26 +48,34 @@ def mvp_found_pct(test_dataset_predictions):
 def avg_real_mvp_rank(test_dataset_predictions):
     metrics = (test_dataset_predictions["REAL_RANK"]).mean()
     return "%.2f" % metrics
+def clean_data(data):
+    #TODO : reuse cleaning process
+    return data.fillna(0.0)
 
 def predict(data, model):
     cat = ['POS', 'CONF']
     num = ['2P%', '2P_per_game', '3P%', '3PAR_advanced', '3PA_per_game', 'AGE', 'AST%_advanced', 'BLK_per_36min', 'DBPM_advanced', 'DRB_per_game', 'DRTG_per_100poss', 'DWS_advanced', 'FG%', 'FG_per_100poss', 'FT%', 'FTR_advanced', 'FT_per_game', 'G', 'MP', 'OBPM_advanced', 'ORB%_advanced', 'ORTG_per_100poss', 'OWS_advanced', 'PF_per_36min', 'PF_per_game', 'PTS_per_game', 'STL_per_game', 'TOV%_advanced', 'TOV_per_36min', 'TOV_per_game', 'TRB_per_36min', 'TS%_advanced', 'WS/48_advanced', 'GB', 'PW', 'PL', 'PA/G', 'CONF_RANK']
     min_max_scaling = True
-    data_processed_features_only, data_raw = preprocess.scale_per_value_of(data, cat, num, data["SEASON"], min_max_scaler=min_max_scaling)
+    data_processed_features_only, _ = preprocess.scale_per_value_of(data, cat, num, data["SEASON"], min_max_scaler=min_max_scaling)
     features = ['OWS_advanced', 'ORTG_per_100poss', 'STL_per_game', 'PW', 'TOV%_advanced', 'FG_per_100poss', 'DRTG_per_100poss', 'TOV_per_36min', 'PF_per_36min', 'CONF_RANK', 'OBPM_advanced', 'ORB%_advanced', 'FT%', 'DBPM_advanced', 'PTS_per_game', 'FTR_advanced', 'BLK_per_36min', 'FT_per_game', 'MP', 'AST%_advanced', '3PAR_advanced', 'PF_per_game', '2P_per_game', 'FG%', 'TOV_per_game', 'DRB_per_game', 'TS%_advanced', 'PA/G', 'PL', '2P%', 'DWS_advanced', '3PA_per_game', 'GB', 'TRB_per_36min', '3P%', 'AGE', 'G', 'WS/48_advanced', 'POS_C', 'POS_PF', 'POS_PG', 'POS_SF', 'POS_SG', 'CONF_EASTERN_CONF', 'CONF_WESTERN_CONF']
     X = data_processed_features_only[features]
     preds = model.predict(X)
+    return preds
 
 # Init page
-current_team_stats = load_team_stats(year)
-current_player_stats = load_player_stats(year)
+current_team_stats = load_team_stats(year).copy()
+current_player_stats = load_player_stats(year).copy()
 current_consolidated_raw = consolidate_stats(current_team_stats, current_player_stats)
 preds_2020 = load_2020_preds()
 preds_test = load_test_preds()
 num_test_seasons = len(preds_test)
 mvp_found_pct = mvp_found_pct(preds_test)
 avg_real_mvp_rank = avg_real_mvp_rank(preds_test)
-model = joblib.load('static/model/model.joblib') 
+model = joblib.load('static/model/model.joblib')
+dataset = clean_data(current_consolidated_raw)
+predictions = predict(dataset, model)
+dataset["PREDS"] = predictions
+dataset = dataset.sort_values(by="PREDS", ascending=False)
 
 # Sidebar
 st.sidebar.image(logo_url, width=100, clamp=False, channels='RGB', output_format='auto')
@@ -82,6 +92,7 @@ Expected performance of the model, as calculated on the test set ({num_test_seas
 
 # Main content
 st.header("Current year predictions")
+st.dataframe(data=dataset.head(10), width=None, height=None)
 st.header("Data retrieved")
 st.subheader("Player stats")
 st.markdown('''
