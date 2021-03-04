e5ef9dd8048d416afb033ac6fbd6cc905454104b
pauldes <pauldesigaud@outlook.fr>
2021-03-04 14:01:11 +0100

    Lint preprocess.py

--- nba/preprocess.py
+++ nba/preprocess.py
@@ -1,5 +1,3 @@
-import random
-
 import pandas
 import numpy
 import sklearn
@@ -27,32 +25,72 @@ def standardize(dataframe, fit_on=None, fit_per_values_of=None, min_max_scaler=F
     return scaled
 
 def get_numerical_columns(dataframe):
+    """ Get columns of type number
+
+    Args:
+        dataframe (pandas.DataFrame): Data frame
+
+    Returns:
+        list[str]: Columns
+    """
     return dataframe.select_dtypes(include="number").columns
 
 def get_categorical_columns(dataframe):
+    """ Get columns of type non-number
+
+    Args:
+        dataframe (pandas.DataFrame): Data frame
+
+    Returns:
+        list[str]: Columns
+    """
     return dataframe.select_dtypes(exclude="number").columns
 
 def natural_log_transform(series):
+    """ Perform natural log transformation
+
+    Args:
+        series (pandas.Series): Series to transform
+
+    Returns:
+        pandas.Series: Transformed series
+    """
     return numpy.log(series+1)
 
 def exp_transform(series):
+    """ Perform exponential transformation
+
+    Args:
+        series (pandas.Series): Series to transform
+
+    Returns:
+        pandas.Series: Transformed series
+    """
     return numpy.exp(series)
 
-def select_random_unique_values(series, share:float):
-    share_int = int(share*len(series.unique()))
-    seasons = series.sample(share_int).tolist()
-    print(share_int)
-    return seasons
+def select_random_unique_values(series:pandas.Series, share:float):
+    """ Select a set of random unique values
 
-def scale_per_value_of(data, selected_cat_features, selected_num_features, scale_per_value_of, min_max_scaler=True):
+    Args:
+        series (pandas.Series): Series to select values from
+        share (float): Share of unique values to select
+
+    Returns:
+        list[]: List of unique values
+    """
+    share_int = int(share*len(series.unique()))
+    sample = series.sample(share_int).tolist()
+    return sample
+
+def scale_per_value_of(data, selected_cat_features, selected_num_features, fit_per_value_of, min_max_scaler=True):
     if selected_num_features is None or len(selected_num_features)==0:
         raise NotImplementedError("Need at least 1 numerical feature")
-    X_num = standardize(data[selected_num_features], fit_on=None, fit_per_values_of=scale_per_value_of, min_max_scaler=min_max_scaler)
+    processed_num_data = standardize(data[selected_num_features], fit_on=None, fit_per_values_of=fit_per_value_of, min_max_scaler=min_max_scaler)
     if selected_cat_features is not None and len(selected_cat_features)>0:
-        X_cat = pandas.get_dummies(data[selected_cat_features]) #, drop_first=True)
-        X_processed = pandas.concat([X_num, X_cat], axis=1)
-        X_raw = data[selected_num_features + selected_cat_features]
+        processed_cat_data = pandas.get_dummies(data[selected_cat_features]) #, drop_first=True)
+        processed_data = pandas.concat([processed_num_data, processed_cat_data], axis=1)
+        raw_data = data[selected_num_features + selected_cat_features]
     else:
-        X_processed = X_num
-        X_raw = data[selected_num_features]
-    return X_processed, X_raw
+        processed_data = processed_num_data
+        raw_data = data[selected_num_features]
+    return processed_data, raw_data
