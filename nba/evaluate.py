be4e215bb5b14d43d13e573be7ea9003234ce558
pauldes <pauldesigaud@outlook.fr>
2021-03-04 14:14:56 +0100

    Lint evaulate.py

--- nba/evaluate.py
+++ nba/evaluate.py
@@ -1,10 +1,26 @@
 import pandas
 import numpy
 
-def softmax(series):
-    """Compute softmax values for each sets of scores in x."""
+def softmax(series:pandas.Series):
+    """ Compute softmax values for each sets of scores in x.
+
+    Args:
+        series (pandas.Series): Series
+
+    Returns:
+        pandas.Series: Transformed series (total is 1)
+    """
     e_x = numpy.exp(series - numpy.max(series))
     return e_x / e_x.sum()
 
-def share(series):
+def share(series:pandas.Series):
+    """ Compute the share of each value in the series by its share of the series total
+
+    Args:
+        series (pandas.Series): Series
+
+    Returns:
+        pandas.Series: Series of shares (total is 1)
+    """
     return series / series.sum()
+    
