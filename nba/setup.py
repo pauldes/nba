ae4dc1bebabe8032659e6c7286639a90664a69f4
pauldes <pauldesigaud@outlook.fr>
2021-03-07 19:22:34 +0100

    Add basic setup

---
+++ nba/setup.py
@@ -0,0 +1,9 @@
+import setuptools
+
+setuptools.setup(
+    name="nba",
+    version="0.0.1",
+    description="Package used for the application nba-mvp-prediction",
+    packages=[],
+    python_requires='==3.7.9',
+)
