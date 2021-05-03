700d178960dc8121b7614c471a43c33bb41e515a
JW6111 <paul.desigaud@external.engie.com>
2021-05-03 17:56:30 +0200

    Add transparency

--- app_web.py
+++ app_web.py
@@ -594,7 +594,7 @@ if navigation_page == PAGE_PREDICTIONS:
     Impacts (SHAP values) are relative to the top-10 predicted MVP candidates. These values may not be reliable for categorical variables (as demonstrated [here](https://arxiv.org/pdf/2103.13342.pdf) and [here](https://arxiv.org/pdf/1909.08128.pdf)).
     """
     )
-    st.pyplot(fig, width=None, height=100)
+    st.pyplot(fig, transparent=True, width=None, height=100)
 
 elif navigation_page == PAGE_PERFORMANCE:
 
