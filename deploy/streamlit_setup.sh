a92bd17b2dbf8fa1c77066aa74a8128d3154684f
JW6111 <paul.desigaud@external.engie.com>
2021-05-03 16:39:32 +0200

    Disable watchdog

--- deploy/streamlit_setup.sh
+++ deploy/streamlit_setup.sh
@@ -6,8 +6,13 @@ email = \"dummy@email.com\"\n\
 " > ~/.streamlit/credentials.toml
 
 echo "\
+
+[global]\
+disableWatchdogWarning = false\
+sharingMode = "off"\
 [server]\n\
 headless = true\n\
 enableCORS=false\n\
+watchFileSystem = false\n\
 port = $PORT\n\
 " > ~/.streamlit/config.toml
